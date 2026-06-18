218eaf2aaef1fba9beeb85e8f17d6b3c46eabfb2
ByakuranN7 <51470781+ByakuranN7@users.noreply.github.com>
2026-06-18 07:33:56 -0300

    feat: adiciona exercício sobre operadores de incremento e decremento

---
+++ Exercicios/Seção 12 - Operadores/IncrementoDecremento.java
@@ -0,0 +1,13 @@
+public class IncrementoDecremento {
+    public static void main(String[] args) {
+        int contador = 5;
+
+        System.out.println("Valor inicial: " + contador);
+
+        contador++;
+        System.out.println("Após incremento (++): " + contador);
+
+        contador--;
+        System.out.println("Após decremento (--): " + contador);
+    }
+}
